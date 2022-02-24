05df4d0614783bc6de00219672c3409573087ac3
Moritz Roessler <moritz.roessler@gmail.com>
2022-02-24 11:05:00 +0100

    fix: add babel react support

---
+++ dist/components/TestComponent.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.TestComponent = void 0;
+
+var _jsxRuntime = require("react-server/jsx-renderer/jsx-runtime");
+
+const TestComponent = () => {
+  return (0, _jsxRuntime.jsx)("div", {
+    children: "Hello"
+  });
+};
+
+exports.TestComponent = TestComponent;
--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,18 @@
-export {};
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _TestComponent = require("./components/TestComponent");
+
+Object.keys(_TestComponent).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (key in exports && exports[key] === _TestComponent[key]) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function () {
+      return _TestComponent[key];
+    }
+  });
+});
